06828681839bdfdb35f57ab09eec0a68c47a97e0
Bruno <ballem.bruno@gmail.com>
2017-02-04 15:49:07 -0200

    Comitando projeto

---
+++ src/main/java/com/brunoballem/loja_virtual_jornada/App.java
@@ -0,0 +1,13 @@
+package com.brunoballem.loja_virtual_jornada;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
